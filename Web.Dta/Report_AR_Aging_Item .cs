2476369f626a427476b2b5c1541cbcbf11e5554a
Rahma <betawikoe@gmail.com>
2023-04-03 09:27:15 +0700

    AR

--- Web.Dta/Report_AR_Aging_Item .cs
+++ Web.Dta/Report_AR_Aging_Item .cs
@@ -20,14 +20,14 @@ namespace Web.Dta
             context.CommandType = CommandType.StoredProcedure;
             return DBUtil.ExecuteMapper(context, new Report_AR_Aging_Dbo());
         }
-        public static List<Report_ARDbo> GetDetail(string custid)
+        public static List<Report_AR_Aging_Dbo> GetDetail(string custid)
         {
             IDBHelper context = new DBHelper();
             string sqlQuery = "sp_report_AR_Aging_Detail";
             context.AddParameter("param", custid); //sementara hardcode
             context.CommandText = sqlQuery;
             context.CommandType = CommandType.StoredProcedure;
-            return DBUtil.ExecuteMapper(context, new Report_ARDbo());
+            return DBUtil.ExecuteMapper(context, new Report_AR_Aging_Dbo());
         }
 
         #region Data Access     
