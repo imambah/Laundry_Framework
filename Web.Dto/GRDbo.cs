61df2afef07e659b379c2a802a9d13dd171c9975
imambah <imam.bahtiar@gmail.com>
2023-05-28 20:25:22 +0700

    Tambah no GR di grid

--- Web.Dto/GRDbo.cs
+++ Web.Dto/GRDbo.cs
@@ -7,6 +7,7 @@ namespace Web.Dto
     {
         public int RecordID { get; set; }
         public string PO_Number { get; set; }
+        public string GR_Number { get; set; }
         public string supplier_id { get; set; }
         public string supplier_name { get; set; }
         public DateTime? tgl_po { get; set; }
@@ -15,6 +16,7 @@ namespace Web.Dto
             GRDbo obj = new GRDbo();
             obj.RecordID = Convert.ToInt32(reader["RecordID"]);
             obj.PO_Number = reader["PO_Number"] == DBNull.Value ? "" : reader["PO_Number"].ToString();
+            obj.GR_Number = reader["GR_Number"] == DBNull.Value ? "" : reader["GR_Number"].ToString();
             obj.supplier_id = reader["supplier_id"] == DBNull.Value ? "" : reader["supplier_id"].ToString();
             obj.supplier_name = reader["supplier_name"] == DBNull.Value ? "" : reader["supplier_name"].ToString();
             obj.tgl_po = reader["tgl_po"] == DBNull.Value ? (DateTime?)null : Convert.ToDateTime(reader["tgl_po"]);
